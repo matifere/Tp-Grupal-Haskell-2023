efaab988e80c3b4b8e1342110a4785da70e3e239
GuillermoCzza <guiczza@gmail.com>
2023-04-29 13:01:07 -0300

    comment many excercises

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -39,35 +39,35 @@ likesDePublicacion (_, _, us) = us
 nombresDeUsuarios :: RedSocial -> [String]
 nombresDeUsuarios = undefined
 
--- describir qué hace la función: .....
+-- describir qué hace la función: devuelve la lista de amigos de un usuario dado de una red social dada.
 amigosDe :: RedSocial -> Usuario -> [Usuario]
 amigosDe = undefined
 
--- describir qué hace la función: .....
+-- describir qué hace la función: devuelve la cantidad de amigos de un usuario dado de una red social dada.
 cantidadDeAmigos :: RedSocial -> Usuario -> Int
 cantidadDeAmigos = undefined
 
--- describir qué hace la función: .....
+-- describir qué hace la función: devuelve al usuario con más amigos de una red social dada.
 usuarioConMasAmigos :: RedSocial -> Usuario
 usuarioConMasAmigos = undefined
 
--- describir qué hace la función: .....
+-- describir qué hace la función: devuelve True si la red social dada tiene algún usuario con más (mayor estricto) de 1000000 amigos. Sino devuelve False.
 estaRobertoCarlos :: RedSocial -> Bool
 estaRobertoCarlos = undefined
 
--- describir qué hace la función: .....
+-- describir qué hace la función: devuelve la lista de publicaciones de un usuario dado de una red social dada
 publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
 publicacionesDe = undefined
 
--- describir qué hace la función: .....
+-- describir qué hace la función: devuelve la lista de publicaciones de una red social dada que le gustan a un usuario dado
 publicacionesQueLeGustanA :: RedSocial -> Usuario -> [Publicacion]
 publicacionesQueLeGustanA = undefined
 
--- describir qué hace la función: .....
+-- describir qué hace la función: devuelve True si las listas de publicaciones que les gustan a dos usuarios son idénticas (esto está bien interpretado de la especificación? creo que sí)
 lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
 lesGustanLasMismasPublicaciones = undefined
 
--- describir qué hace la función: devuelve True si existe otro usuario en la red social provista distinto al usuario provisto que dio like a todas las publicaciones de usuario provisto (n° publicaciones > 0). Sino devuelve False.
+-- describir qué hace la función: devuelve True si existe otro usuario en la red social dada que dio like a todas las publicaciones de usuario dados (n° publicaciones > 0). Sino devuelve False.
 tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
 tieneUnSeguidorFiel = undefined
 
